4aaaf57e8fd1edfeddfa4e41f91c803879a831ef
monkey <vasilevskiy.job@gmail.com>
2022-09-04 13:58:42 +0300

    add package resorces

--- app/methods_change_file.py
+++ app/methods_change_file.py
@@ -1,12 +1,16 @@
 import io
+from importlib.resources import open_binary
 
 import docx
 
+from app import resorces
+
 
 async def get_file(starting_point: str, end_point: str, date: str) -> io.BytesIO:
+    document = open_binary(resorces, 'dad_info.docx')
     output = io.BytesIO()
     output.name = 'file_dad.docx'
-    doc = docx.Document('dad_info.docx')
+    doc = docx.Document(document)
     doc.paragraphs[-2].text = f'Путевой: б/н        от {date}'
     doc.paragraphs[-1].text = 'Маршрут                 '
     doc.paragraphs[-1].add_run(f'{starting_point.title()}-{end_point.title()}').bold = True
